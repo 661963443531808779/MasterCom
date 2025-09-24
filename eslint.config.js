6dd624226420848f5c4385da10aef2a21878cef8
Dorian <136206694+661963443531808779@users.noreply.github.com>
2025-09-24 02:06:01 +0200

    Fix of warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,11 @@
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default [
+export default tseslint.config(
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     ignores: [
       'dist/**',
@@ -22,10 +26,26 @@ export default [
         document: 'readonly',
         process: 'readonly'
       },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
       'no-unused-vars': 'off', // Désactivé pour TypeScript
       'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-expressions': 'off', // Désactivé pour éviter les conflits
     },
   },
-];
+);
